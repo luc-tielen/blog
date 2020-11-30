26058781b54255b220e3518dbcd4e06506d7607a
Luc Tielen <luc.tielen@kabisa.nl>
2020-11-30 08:03:59 +0100

    Use slick template

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Blog
 
 This repository contains the content for my personal blog.
+
+## Fonts
+
+TODO pick 2 fonts:
+
+- Oswald
+- https://www.notion.so/Lekton-ef8f4120b54f444d82fda4b491b23567
+- https://www.notion.so/Sunflower-e374a80af18c44cea7fdb968cdc04046
+- https://fonts.google.com/specimen/Oxygen#license
+- https://fonts.google.com/specimen/Nunito?query=nunito
+- https://www.notion.so/Saira-a2f246a9e4b9428283d07f676ba5fc0e
+- https://www.notion.so/Barlow-f39c4335473d4e888593fd0e89f00148
+- https://www.notion.so/Manrope-9e8eb091cc2e46b189b7cb663925f72e
---
+++ app/Main.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE NamedFieldPuns        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Main where
+
+import           Control.Lens
+import           Control.Monad
+import           Data.Aeson                 as A
+import           Data.Aeson.Lens
+import           Data.Time
+import           Development.Shake
+import           Development.Shake.Classes
+import           Development.Shake.Forward
+import           Development.Shake.FilePath
+import           GHC.Generics               (Generic)
+import           Slick
+
+import qualified Data.HashMap.Lazy as HML
+import qualified Data.Text                  as T
+
+---Config-----------------------------------------------------------------------
+
+siteMeta :: SiteMeta
+siteMeta =
+    SiteMeta { siteAuthor = "Me"
+             , baseUrl = "https://example.com"
+             , siteTitle = "My Slick Site"
+             , twitterHandle = Just "myslickhandle"
+             , githubUser = Just "myslickgithubuser"
+             }
+
+outputFolder :: FilePath
+outputFolder = "docs/"
+
+--Data models-------------------------------------------------------------------
+
+withSiteMeta :: Value -> Value
+withSiteMeta (Object obj) = Object $ HML.union obj siteMetaObj
+  where
+    Object siteMetaObj = toJSON siteMeta
+withSiteMeta _ = error "only add site meta to objects"
+
+data SiteMeta =
+    SiteMeta { siteAuthor    :: String
+             , baseUrl       :: String -- e.g. https://example.ca
+             , siteTitle     :: String
+             , twitterHandle :: Maybe String -- Without @
+             , githubUser    :: Maybe String
+             }
+    deriving (Generic, Eq, Ord, Show, ToJSON)
+
+-- | Data for the index page
+data IndexInfo =
+  IndexInfo
+    { posts :: [Post]
+    } deriving (Generic, Show, FromJSON, ToJSON)
+
+type Tag = String
+
+-- | Data for a blog post
+data Post =
+    Post { title       :: String
+         , author      :: String
+         , content     :: String
+         , url         :: String
+         , date        :: String
+         , tags        :: [Tag]
+         , description :: String
+         , image       :: Maybe String
+         }
+    deriving (Generic, Eq, Ord, Show, FromJSON, ToJSON, Binary)
+
+data AtomData =
+  AtomData { title        :: String
+           , domain       :: String
+           , author       :: String
+           , posts        :: [Post]
+           , currentTime  :: String
+           , atomUrl      :: String } deriving (Generic, ToJSON, Eq, Ord, Show)
+
+-- | given a list of posts this will build a table of contents
+buildIndex :: [Post] -> Action ()
+buildIndex posts' = do
+  indexT <- compileTemplate' "site/templates/index.html"
+  let indexInfo = IndexInfo {posts = posts'}
+      indexHTML = T.unpack $ substitute indexT (withSiteMeta $ toJSON indexInfo)
+  writeFile' (outputFolder </> "index.html") indexHTML
+
+-- | Find and build all posts
+buildPosts :: Action [Post]
+buildPosts = do
+  pPaths <- getDirectoryFiles "." ["site/posts//*.md"]
+  forP pPaths buildPost
+
+-- | Load a post, process metadata, write it to output, then return the post object
+-- Detects changes to either post content or template
+buildPost :: FilePath -> Action Post
+buildPost srcPath = cacheAction ("build" :: T.Text, srcPath) $ do
+  liftIO . putStrLn $ "Rebuilding post: " <> srcPath
+  postContent <- readFile' srcPath
+  -- load post content and metadata as JSON blob
+  postData <- markdownToHTML . T.pack $ postContent
+  let postUrl = T.pack . dropDirectory1 $ srcPath -<.> "html"
+      withPostUrl = _Object . at "url" ?~ String postUrl
+  -- Add additional metadata we've been able to compute
+  let fullPostData = withSiteMeta . withPostUrl $ postData
+  template <- compileTemplate' "site/templates/post.html"
+  writeFile' (outputFolder </> T.unpack postUrl) . T.unpack $ substitute template fullPostData
+  convert fullPostData
+
+-- | Copy all static files from the listed folders to their destination
+copyStaticFiles :: Action ()
+copyStaticFiles = do
+    filepaths <- getDirectoryFiles "./site/" ["images//*", "css//*", "js//*"]
+    void $ forP filepaths $ \filepath ->
+        copyFileChanged ("site" </> filepath) (outputFolder </> filepath)
+
+formatDate :: String -> String
+formatDate humanDate = toIsoDate parsedTime
+  where
+    parsedTime =
+      parseTimeOrError True defaultTimeLocale "%b %e, %Y" humanDate :: UTCTime
+
+rfc3339 :: Maybe String
+rfc3339 = Just "%H:%M:SZ"
+
+toIsoDate :: UTCTime -> String
+toIsoDate = formatTime defaultTimeLocale (iso8601DateFormat rfc3339)
+
+buildFeed :: [Post] -> Action ()
+buildFeed posts = do
+  now <- liftIO getCurrentTime
+  let atomData =
+        AtomData
+          { title = siteTitle siteMeta
+          , domain = baseUrl siteMeta
+          , author = siteAuthor siteMeta
+          , posts = mkAtomPost <$> posts
+          , currentTime = toIsoDate now
+          , atomUrl = "/atom.xml"
+          }
+  atomTempl <- compileTemplate' "site/templates/atom.xml"
+  writeFile' (outputFolder </> "atom.xml") . T.unpack $ substitute atomTempl (toJSON atomData)
+    where
+      mkAtomPost :: Post -> Post
+      mkAtomPost p = p { date = formatDate $ date p }
+
+-- | Specific build rules for the Shake system
+--   defines workflow to build the website
+buildRules :: Action ()
+buildRules = do
+  allPosts <- buildPosts
+  buildIndex allPosts
+  buildFeed allPosts
+  copyStaticFiles
+
+main :: IO ()
+main = do
+  let shOpts = shakeOptions { shakeVerbosity = Chatty, shakeLintInside = ["\\"]}
+  shakeArgsForward shOpts buildRules
